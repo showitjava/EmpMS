d1af5d37ef8699d8c60e7567bf1e8e2a9b08d230
Bhuvnesh <bhuvnesh2019@gmail.com>
2025-12-11 05:10:30 +0530

    changes in controller after delpoying to aws

--- src/main/java/com/example/EmpMS/controller/EmployeeController.java
+++ src/main/java/com/example/EmpMS/controller/EmployeeController.java
@@ -91,7 +91,7 @@ public class EmployeeController {
     }*/
     @GetMapping("/testing")
     public String testing(){
-        return "Testing controller";
+        return "this  controller is working fine and successfully deploy my changes to AWS";
     }
 
 }
